30158cafb8fe2986372b246896fedaced8da7ec2
railsnjoyer <railsenjoyer@gmail.com>
2025-03-18 17:48:43 +0100

    list of buyings

---
+++ lessons/list_of_buyings.rb
@@ -0,0 +1,18 @@
+goods_shop = {
+  soap: 2,
+  light_bulb: 1,
+  spoon: 5,
+  flover: 10,
+  towel: 3
+}
+
+food_shop = {
+  egg: 10,
+  water: 1,
+  cheese: 1,
+  tomato: 5,
+  bread: 2
+}
+
+total = grocery_shop.merge(product_shop)
+puts total
